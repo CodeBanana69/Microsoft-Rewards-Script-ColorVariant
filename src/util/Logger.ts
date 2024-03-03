d52c5105ce95a8eff96998aefd097b18b1312302
CodeBanana69 <114698628+CodeBanana69@users.noreply.github.com>
2024-03-03 07:23:33 -0800

    Colors

--- src/util/Logger.ts
+++ src/util/Logger.ts
@@ -1,26 +1,34 @@
-import { Webhook } from './Webhook'
+import { Webhook } from "./Webhook";
 
-export function log(title: string, message: string, type?: 'log' | 'warn' | 'error') {
-    const currentTime = new Date().toLocaleString()
+export function log(
+  title: string,
+  message: string,
+  type?: "log" | "warn" | "error",
+) {
+  const currentTime = new Date().toLocaleString();
 
-    let str = ''
+  let str = "";
+  let color = "";
 
-    switch (type) {
-        case 'warn':
-            str = `[${currentTime}] [PID: ${process.pid}] [WARN] [${title}] ${message}`
-            console.warn(str)
-            break
+  switch (type) {
+    case "warn":
+      str = `[${currentTime}] [PID: ${process.pid}] [WARN] [${title}] ${message}`;
+      color = "\x1b[38;5;214m"; // Orange-ish color
+      console.warn(`${color}${str}\x1b[0m`); 
+      break;
 
-        case 'error':
-            str = `[${currentTime}] [PID: ${process.pid}] [ERROR] [${title}] ${message}`
-            console.error(str)
-            break
+    case "error":
+      str = `[${currentTime}] [PID: ${process.pid}] [ERROR] [${title}] ${message}`;
+      color = "\x1b[38;5;196m"; // Red color
+      console.error(`${color}${str}\x1b[0m`);
+      break;
 
-        default:
-            str = `[${currentTime}] [PID: ${process.pid}] [LOG] [${title}] ${message}`
-            console.log(str)
-            break
-    }
+    default:
+      str = `[${currentTime}] [PID: ${process.pid}] [LOG] [${title}] ${message}`;
+      color = "\x1b[38;5;33m"; // Blue-ish color
+      console.log(`${color}${str}\x1b[0m`); 
+      break;
+  }
 
-    if (str) Webhook(str)
+  if (str) Webhook(str);
 }
